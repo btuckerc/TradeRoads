b097bf7010eb1bc86275cf6c82c11774777b4722
btuckerc <35579682+btuckerc@users.noreply.github.com>
2026-01-03 20:41:40 -0500

    batman

---
+++ Apps/iOS/TradeRoads/TradeRoads/TradeRoadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeRoadsApp.swift
+//  TradeRoads
+//
+//  Created by Tucker Craig on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TradeRoadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/iOS/TradeRoads/TradeRoadsTests/TradeRoadsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TradeRoadsTests.swift
+//  TradeRoadsTests
+//
+//  Created by Tucker Craig on 1/3/26.
+//
+
+import Testing
+@testable import TradeRoads
+
+struct TradeRoadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
